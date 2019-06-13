7d65519c90315225469e909472ded4e4c15cd21d
Chris Bolin <bolin.chris@gmail.com>
2019-06-12 23:33:49 -0600

    client: webpack

---
+++ packages/metrics-client/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./index",
+  output: {
+    path: path.resolve("./dist/"),
+    filename: "index.js",
+    libraryTarget: "commonjs"
+  },
+  target: "browser"
+};
